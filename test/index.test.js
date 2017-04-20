3e29263f407925fbb7b52ec75c9a0db3e4196cec
Alemneh <Alemneh@hotmail.com>
2017-04-19 23:22:44 -0700

    added first test

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+'use strict';
+var expect = require('chai').expect;
+var actions = require('../lib/actions');
+
+describe('Actions', function() {
+  it('should create a json file', function() {
+    expect(true).to.be.true;
+  });
+});
