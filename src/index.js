2ebadb506760143fb2fdf0eba41f86d224845a78
Alemneh <Alemneh@hotmail.com>
2017-04-25 13:28:30 -0700

    fix(Create State function): Fixed where the state was being stored now in root dir.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ const fs = require('fs');
 const ProgressBar = require('progress');
 const chalk = require('chalk');
 const request = require('superagent');
-const co = require('co');
-const prompt = require('co-prompt');
 const program = require('commander');
 const actions = require('./lib/actions');
 const path = require('path');
 const pkg = require('../package.json');
 global.appRoot = path.resolve(__dirname);
+console.log(actions);
 
 
 
@@ -22,37 +21,7 @@ program
   .description('List files and folders')
   .option('-a, --all', 'List all files and folders')
   .option('-l, --long','')
-  .action(function(file, options) {
-    co(function *() {
-      console.log('Applicant Info:');
-      var info = {};
-      info.fullName = yield prompt('full name: ');
-      info.email    = yield prompt('email: ');
-      info.phoneNum = yield prompt('phone: ');
-      info.filePath = file;
-      console.log(info);
-      var state = {
-        userInfo: {
-
-        }
-      };
-
-      state.userInfo = info;
-      var filePath = __dirname + '/data/' + info.filePath + '.json';
-
-      var json = JSON.stringify(state);
-      fs.mkdir(__dirname + '/data', function(err) {
-        if(err) throw err;
-        fs.writeFile(filePath, json, 'utf8', (err) => {
-          if (err) throw err;
-
-          console.log('File saved!');
-          process.exit(0);
-        });
-      });
-
-    });
-  });
+  .action(actions.createState);
 
 program.parse(process.argv);
 
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,31 +1,43 @@
 'use strict';
 const fs = require('fs');
+const co = require('co');
+const prompt = require('co-prompt');
 
 
-function createState(info, options) {
-  console.log('hit');
-  var state = {
-    userInfo: {
 
-    }
-  };
+function createState(file, options) {
+  co(function *() {
+    console.log('Applicant Info:');
+    var info = {};
+    info.fullName = yield prompt('full name: ');
+    info.email    = yield prompt('email: ');
+    info.phoneNum = yield prompt('phone: ');
+    info.filePath = file;
+    console.log(info);
+    var state = {
+      userInfo: {
 
-  state.userInfo = info;
-  var filePath = __dirname + info.filePath + '.json';
+      }
+    };
+    console.log(__dirname);
+    state.userInfo = info;
+    var filePath =  __dirname + '/../data/' + info.filePath + '.json';
 
-  var json = JSON.stringify(state);
-  fs.mkdir(__dirname + '/data', function(err) {
-    if(err) throw err;
-    fs.writeFile(filePath, json, 'utf8', (err) => {
-      if (err) throw err;
+    var json = JSON.stringify(state);
+    fs.mkdir(__dirname + '/../data', function(err) {
+      if(err) throw err;
+      fs.writeFile(filePath, json, 'utf8', (err) => {
+        if (err) throw err;
 
-      console.log('File saved!');
+        console.log('File saved!');
+        process.exit(0);
+      });
     });
-  });
 
+  });
 }
 
 
-module.export = {
+module.exports = {
   createState: createState
 };
